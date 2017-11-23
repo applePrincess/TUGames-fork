4189f2c96b978077a6cbb81ce425861027969f9b
applePrincess <yharuhi396@gmail.com>
2017-11-23 13:37:35 +0000

    Instead of instantiating new brush, use system default.

--- MyForm.cs
+++ MyForm.cs
@@ -5,38 +5,38 @@ using System.Collections.Generic;
 
 class MyForm : System.Windows.Forms.Form
 {
-	protected System.Timers.Timer		mTimer = new System.Timers.Timer();
-	protected System.Drawing.SolidBrush	mSBWhite = new System.Drawing.SolidBrush( System.Drawing.Color.White );
+    protected System.Timers.Timer		mTimer = new System.Timers.Timer();
+    protected System.Drawing.SolidBrush	mSBWhite = (System.Drawing.SolidBrush)System.Drawing.Brushes.White;
 
-	protected override void OnLoad( EventArgs e )
-	{
-		ClientSize = new System.Drawing.Size( 960, 720 );
-		Left = 62;	//	キャプチャ都合上
-		Top = 20;	//	キャプチャ都合上
+    protected override void OnLoad( EventArgs e )
+    {
+        ClientSize = new System.Drawing.Size( 960, 720 );
+        Left = 62;	//	キャプチャ都合上
+        Top = 20;	//	キャプチャ都合上
 
-		DoubleBuffered = true;
-		BackColor = System.Drawing.Color.Black;
+        DoubleBuffered = true;
+        BackColor = System.Drawing.Color.Black;
 
-		mTimer.Elapsed += new System.Timers.ElapsedEventHandler( onMyTimer );
-	}
+        mTimer.Elapsed += new System.Timers.ElapsedEventHandler( onMyTimer );
+    }
 
-	protected override void OnPaint( System.Windows.Forms.PaintEventArgs e )
-	{
-		base.OnPaint( e );
+    protected override void OnPaint( System.Windows.Forms.PaintEventArgs e )
+    {
+        base.OnPaint( e );
 
-		System.Drawing.Graphics	g = e.Graphics;
-		g.ScaleTransform( 8, 8 );
-		g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
-		g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
+        System.Drawing.Graphics	g = e.Graphics;
+        g.ScaleTransform( 8, 8 );
+        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+        g.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half;
 
-		onMyPaint( g );
-	}
+        onMyPaint( g );
+    }
 
-	protected virtual void onMyPaint( System.Drawing.Graphics g )
-	{
-	}
+    protected virtual void onMyPaint( System.Drawing.Graphics g )
+    {
+    }
 
-	protected virtual void onMyTimer( object sender, System.Timers.ElapsedEventArgs e )
-	{
-	}
+    protected virtual void onMyTimer( object sender, System.Timers.ElapsedEventArgs e )
+    {
+    }
 }
